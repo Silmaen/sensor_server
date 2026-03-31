b52d18640bb2263d805cbf652273bf08ec67f69b
Damien Lachouette <damien.lachouette@hexagon.com>
2026-03-31 13:38:50 +0200

    improved UI

--- web/sensor_server/settings.py
+++ web/sensor_server/settings.py
@@ -56,6 +56,7 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
+                "django.template.context_processors.i18n",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "accounts.context_processors.user_role",
